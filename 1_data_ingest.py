4e9576fb82ed1f7334b44ec46cfac9d2ee09c031
CrisSchez <89860535+CrisSchez@users.noreply.github.com>
2022-01-31 17:43:54 +0100

    Update 1_data_ingest.py

--- 1_data_ingest.py
+++ 1_data_ingest.py
@@ -120,8 +120,7 @@ schema = StructType(
 storage = os.environ['STORAGE']
 
 telco_data = spark.read.csv(
-    "{}/datalake/data/churn/WA_Fn-UseC_-Telco-Customer-Churn-.csv".format(
-        storage),
+    "file:/home/cdsw/raw/WA_Fn-UseC_-Telco-Customer-Churn-.csv",
     header=True,
     schema=schema,
     sep=',',
